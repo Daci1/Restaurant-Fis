a092c9531ce90ab4768a48d3531883d0649ae3f4
Daci1 <dacian.barbu28@gmail.com>
2021-04-19 22:35:12 +0300

    Revert :Solved a bug for password and email textbox.

--- src/main/java/org/restaurantfis/sre/Main.java
+++ src/main/java/org/restaurantfis/sre/Main.java
@@ -1,11 +1,10 @@
 package org.restaurantfis.sre;
 
-import org.restaurantfis.sre.model.RegistrationFrame;
 
 public class Main{
     public static void main(String args[])
     {
-        new RegistrationFrame();
+        
     }
 
 }
