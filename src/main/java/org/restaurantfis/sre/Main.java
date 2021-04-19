35e22b53636be2e3e51657b0e3cb9f1644a51ab1
Nicole0027 <nicole_laurian@yahoo.com>
2021-04-19 19:26:26 +0300

    RF-16 added menu GUI

--- src/main/java/org/restaurantfis/sre/Main.java
+++ src/main/java/org/restaurantfis/sre/Main.java
@@ -1,11 +1,11 @@
 package org.restaurantfis.sre;
 
+import org.restaurantfis.sre.model.GUImenu;
+
 public class Main{
     public static void main(String args[])
     {
-        System.out.println("New App");
+    new GUImenu();
     }
 
 }
-
-
