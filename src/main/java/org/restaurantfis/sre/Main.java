042f140b82ce888db689351454699f50ea9e6dca
Nicole0027 <nicole_laurian@yahoo.com>
2021-04-19 18:23:33 +0300

    Remove GUImenu

--- src/main/java/org/restaurantfis/sre/Main.java
+++ src/main/java/org/restaurantfis/sre/Main.java
@@ -1,13 +1,11 @@
 package org.restaurantfis.sre;
 
-import org.restaurantfis.sre.model.GUImenu;
 import org.restaurantfis.sre.model.RegistrationFrame;
 
 public class Main{
     public static void main(String args[])
     {
         new RegistrationFrame();
-        
     }
 
 }
