5ca90f0b2f1cdc5c67e30352bdc43b030eb7d7d8
Nicole0027 <nicole_laurian@yahoo.com>
2021-05-01 19:16:43 +0300

    RF-18 created the edit, add delete and save buttons

--- src/main/java/org/restaurantfis/sre/Main.java
+++ src/main/java/org/restaurantfis/sre/Main.java
@@ -1,19 +1,13 @@
 package org.restaurantfis.sre;
+import org.restaurantfis.sre.model.*;
 
 
-import com.mongodb.*;
-import org.restaurantfis.sre.model.Date;
-import org.restaurantfis.sre.model.RegistrationFrame;
-import org.restaurantfis.sre.model.User;
-import org.restaurantfis.sre.services.UserService;
 
 public class Main{
 
-
-
     public static void main(String args[])
     {
-        UserService.initializeDB();
+        new foodMenuPage();
 
 
     }
---
+++ src/main/java/org/restaurantfis/sre/model/foodMenuPage.java
@@ -0,0 +1,134 @@
+package org.restaurantfis.sre.model;
+import javax.swing.*;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class foodMenuPage extends JFrame implements ActionListener
+{
+    private JLabel page_title ;
+    private Container menu_page ;
+
+    private JTable menu ;
+
+    private Object[] columns = {"FOOD","FOOD PRICE","DRINKS","DRINKS PRICE"};
+    private DefaultTableModel model;
+    private JScrollPane pane ;
+
+    private JTextField food,food_price,drinks,drinks_price;
+    private JButton add_button, delete_button, save_button, edit_button, back_button;
+
+
+    public foodMenuPage()
+    {
+        setTitle("Restaurant Food Menu");
+        setBounds(300, 90, 900, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        menu_page = getContentPane();
+        menu_page.setBackground(Color.gray);
+        menu_page.setLayout(null);
+
+        //titlul
+        page_title = new JLabel("Restaurant Food Menu");
+        page_title.setFont(new Font("Arial", Font.BOLD, 40));
+        page_title.setSize(500, 50);
+        page_title.setLocation(250, 30);
+        menu_page.add(page_title);
+        //tabel
+        model = new DefaultTableModel(columns,4);
+        menu = new JTable(model);
+
+        pane = new JScrollPane(menu);
+        pane.setBounds(175, 200,550,200);
+        menu_page.add(pane);
+
+        //edit button
+        edit_button = new JButton("EDIT");
+        edit_button.setFont(new Font("Arial", Font.PLAIN,15));
+        edit_button.setForeground(Color.black);
+        edit_button.setLocation(50,30);
+        edit_button.setSize(75,50);
+        edit_button.setBackground(Color.gray);
+        edit_button.addActionListener(this);
+        menu_page.add(edit_button);
+
+        
+
+
+
+        //edit text fields
+        food = new JTextField();
+        food_price = new JTextField();
+        drinks = new JTextField() ;
+        drinks_price = new JTextField();
+
+        food.setBounds(50,235,100,35);
+        food_price.setBounds(50,270,100,35);
+        drinks.setBounds(50,305,100,35);
+        drinks_price.setBounds(50,340,100,35);
+
+        food.setVisible(false);
+        food_price.setVisible(false);
+        drinks_price.setVisible(false);
+        drinks.setVisible(false);
+
+        menu_page.add(food_price);
+        menu_page.add(food);
+        menu_page.add(drinks);
+        menu_page.add(drinks_price);
+
+        //edit buttons
+        add_button = new JButton("ADD");
+        delete_button = new JButton("DELETE");
+        save_button = new JButton("SAVE");
+
+        add_button.setBounds(50,100,60,30);
+        delete_button.setBounds(50,135,80,30);
+        save_button.setBounds(50,170,60,30);
+        add_button.setVisible(false);
+        delete_button.setVisible(false);
+        save_button.setVisible(false);
+
+        menu_page.add(add_button);
+        menu_page.add(delete_button);
+        menu_page.add(save_button);
+
+
+
+
+
+
+        setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getActionCommand().equals("EDIT"))
+        {
+            food.setVisible(true);
+            food_price.setVisible(true);
+            drinks_price.setVisible(true);
+            drinks.setVisible(true);
+
+            add_button.setVisible(true);
+            delete_button.setVisible(true);
+            save_button.setVisible(true);
+        }
+
+    }
+
+
+
+
+
+
+}
